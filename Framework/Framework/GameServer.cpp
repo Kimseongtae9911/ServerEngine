e92d88d31d413558f4e63f0d90daae7e8753cc71
Kim Seongtae <seongtae07@gmail.com>
2024-07-04 01:25:42 +0900

    객체 생성/해제하지 않는 재사용 오브젝트풀 추가

--- Framework/Framework/GameServer.cpp
+++ Framework/Framework/GameServer.cpp
@@ -4,35 +4,38 @@
 #include "CoreMacro.h"
 #include "ThreadManager.h"
 #include "Memory.h"
+#include "ObjectPool.h"
 
-class Knight
+class Knight : public ReuseObject
 {
 public:
+    void ResetObject() override {
+        std::cout << "Resetting MyObject" << std::endl;
+    }
+
+private:
+    void InitObjectImpl() {}
+
+    // 가변 인자를 받는 InitObject 구현 (다양한 인자)
+    template<typename... Args>
+    void InitObjectImpl(Args&&... args) {
+        // 여러 인자를 출력
+        (std::cout << ... << args) << std::endl;
+    }
+
     int64 hp = 0;
-};
-
-class Monster
-{
-public:
-    int64 id = 0;
 };
 
 int main()
 {
-    Knight* knights[100];
-
-    for (int i = 0; i < 100; ++i) {
-        knights[i] = ObjectPool<Knight>::PopObject();
+    {
+        auto k = ReuseObjectPool<Knight>::MakeObject();
     }
 
-    for (int i = 0; i < 100; ++i) {
-        ObjectPool<Knight>::PushObject(knights[i]);
-        knights[i] = nullptr;
+    {
+        auto k = ReuseObjectPool<Knight>::MakeObject();
     }
-    std::shared_ptr<Knight> ptr = ObjectPool<Knight>::MakeShared();
 
-    Knight* k = ObjectPool<Knight>::PopObject();
-    ObjectPool<Knight>::PushObject(k);
 
     return 0;
 }
--- Framework/ServerCore/ObjectPool.h
+++ Framework/ServerCore/ObjectPool.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Types.h"
 #include "MemoryPool.h"
+#include <tbb/concurrent_queue.h>
 
 /*
 	각 클래스별 고유의 오브젝트 풀을 가지며, 각 오브젝트 풀은 메모리풀을 가짐	
@@ -9,9 +10,14 @@
 	오브젝트 자체는 매번 생성과 해제를 반복함
 */
 template<typename Type>
-class ObjectPool
+class ObjectPool	
 {
 public:
+	static std::shared_ptr<Type> MakeObject() {
+		return std::shared_ptr<Type>(PopObject(), PushObject);
+	}
+
+private:
 	template<typename... Args>
 	static Type* PopObject(Args&&... args) {
 #ifdef _STOMP
@@ -32,10 +38,6 @@ public:
 		s_pool.PushMemory(MemoryHeader::DetachHeader(_obj));
 #endif
 	}
-
-	static std::shared_ptr<Type> MakeShared() {
-		return std::shared_ptr<Type>(PopObject(), PushObject);
-	}
 	 
 private:
 	static int32 s_allocSize;
@@ -49,10 +51,85 @@ template<typename Type>
 MemoryPool ObjectPool<Type>::s_pool{ s_allocSize };
 
 /*
-	todo: 객체 생성해제를 하지 않고 메모리풀을 사용하는 오브젝트풀
+	객체 생성해제를 하지 않고 메모리풀을 사용하는 오브젝트풀
 */
+class ReuseObject
+{
+public:
+	template<typename... Args>
+	void InitObject(Args&&... args) { InitObjectImpl(std::forward<Args>(args)...); }
+
+	virtual void ResetObject() = 0;	
+private:
+	// 가변 인자를 받는 순수 가상 함수
+	virtual void InitObjectImpl() = 0;	// 가변인자를 받는 함수는 가상함수로 설정이 안됨... 인자 없는거라도 강제..
+};
+
 template<typename Type>
 class ReuseObjectPool
 {
+public:
+	static std::shared_ptr<Type> MakeObject() {
+		return std::shared_ptr<Type>(PopObject(), PushObject);
+	}
 
+	//오브젝트풀을 초기화, 메모리풀은 유지
+	static void ResetObjectPool() {
+		while (!s_objectPool.empty()) {
+			Type* obj = nullptr;
+			s_objectPool.try_pop(obj);
+			obj->~Type();
+#ifdef _STOMP		
+			StompAllocator::Release(MemoryHeader::DetachHeader(obj));
+#else
+			s_pool.PushMemory(MemoryHeader::DetachHeader(obj));
+#endif
+		}
+	}
+
+private:
+	template<typename... Args>
+	static Type* PopObject(Args&&... args) {
+		Type* obj = nullptr;
+		if (s_objectPool.try_pop(obj)) {
+			obj->InitObject(std::forward<Args>(args)...);
+			return obj;
+		}
+
+		//객체가 없다면 생성
+#ifdef _STOMP
+		MemoryHeader* ptr = reinterpret_cast<MemoryHeader*>(StompAllocator::Alloc(s_allocSize));
+		obj = static_cast<Type*>(MemoryHeader::AttachHeader(ptr, s_allocSize));
+#else
+		obj = static_cast<Type*>(MemoryHeader::AttachHeader(s_pool.PopMemory(), s_allocSize));
+#endif
+		new(obj)Type();
+		obj->InitObject(std::forward<Args>(args)...);
+
+		// 타입 검사 ReuseObject를 상속받는지
+		if (typeid(*obj) != typeid(ReuseObject) && !std::is_base_of<ReuseObject, Type>::value) {
+			ASSERT_CRASH("Not Valid Type");	//일단 크래시...메모리 해제하고 nullptr리턴?
+		}
+
+		return obj;
+	}
+
+	static void PushObject(Type* _obj) {
+		_obj->ResetObject();
+		s_objectPool.push(_obj);
+	}
+
+private:
+	static int32 s_allocSize;
+	static MemoryPool s_pool;
+	static tbb::concurrent_queue<Type*> s_objectPool;
 };
+
+template<typename Type>
+int32 ReuseObjectPool<Type>::s_allocSize = sizeof(Type) + sizeof(MemoryHeader);
+
+template<typename Type>
+MemoryPool ReuseObjectPool<Type>::s_pool{ s_allocSize };
+
+template<typename Type>
+tbb::concurrent_queue<Type*> ReuseObjectPool<Type>::s_objectPool;
