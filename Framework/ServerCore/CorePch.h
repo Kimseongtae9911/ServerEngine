89071d774153a64ac2816977216067d40ffe200e
Kim Seongtae <seongtae07@gmail.com>
2024-07-07 15:54:45 +0900

    boost 라이브러리

--- Framework/ServerCore/CorePch.h
+++ Framework/ServerCore/CorePch.h
@@ -5,10 +5,12 @@
 #include "CoreTLS.h"
 #include "CoreGlobal.h"
 
-#include <windows.h> //Linux에서 사용한다면 제외...
 #include <iostream>
 
 #include "Lock.h"
 
 #include "ObjectPool.h"
 #include "TypeCast.h"
+
+//boost
+#include <boost/asio.hpp>
